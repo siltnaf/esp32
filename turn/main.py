ead8de452e196836384517f29ab51c757c52cdc7
siltnaf <kenwong_mailbox@yahoo.com.hk>
2021-02-24 02:24:06 +0800

    setup micropython pycharm

---
+++ turn/main.py
@@ -0,0 +1,9 @@
+import time
+from machine import Pin
+led=Pin(22,Pin.OUT)        #create LED object from pin2,Set Pin2 to output
+
+while True:
+  led.value(1)            #Set led turn on
+  time.sleep(0.5)
+  led.value(0)            #Set led turn off
+  time.sleep(0.5)
