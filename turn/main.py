54541e1c7faaeb49205bf81a4bfaa1f5cd9774e6
siltnaf <kenwong_mailbox@yahoo.com.hk>
2021-02-24 03:13:51 +0800

    test blink time2

--- turn/main.py
+++ turn/main.py
@@ -4,6 +4,6 @@ led=Pin(22,Pin.OUT)        #create LED object from pin2,Set Pin2 to output
 
 while True:
   led.value(1)            #Set led turn on
-  time.sleep(0.5)
+  time.sleep(1)
   led.value(0)            #Set led turn off
   time.sleep(1)
