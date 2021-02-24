1c2a501b4781e31b9cbd60fc21ca9c53e29275c4
siltnaf <kenwong_mailbox@yahoo.com.hk>
2021-02-25 01:34:39 +0800

    test blink time4

--- turn/main.py
+++ turn/main.py
@@ -1,16 +1,52 @@
+
 ##hardware platform: FireBeetle-ESP32
+
+
 #Result: Blink
+
+
 #The information below shows blink is unavailble for the current version.
+
+
 #IO0 IO4 IO10 IO12~19 IO21~23 IO25~27
+
+
 #Except the connection between IO2 and onboard LED, other pins need to connect to external LEDs.
 
+
+
+
+
 from time import sleep
+
+
 from machine import Pin
 
+from mylib import mywifi
+
+
+
+
+ssid="ken_5G"
+password="1233211234"
+
+mywifi.connect(ssid,password)
 
 led=Pin(22,Pin.OUT)        #create LED object from pin2,Set Pin2 to output
 
+
+
 while True:
+
+
   led.value(not led.value())
-  sleep(1.5)
+
+
+  sleep(0.5)
+
+
+
+
+
+
 
--- turn/mylib/mywifi.py
+++ turn/mylib/mywifi.py
@@ -1,13 +1,52 @@
-import network
 
-def connect(ssid,passwd):
-    station=network.WLAN(network.STA_IF)
-    if station.isconnected()==True:
-        print("Already connected")
-        return
-    station.active(True)
-    station.connect(ssid,passwd)
-    while station.isconnected()==False:
-        pass
-    print('connection successful')
-    print(station.ifconfig())
+##hardware platform: FireBeetle-ESP32
+
+
+#Result: Blink
+
+
+#The information below shows blink is unavailble for the current version.
+
+
+#IO0 IO4 IO10 IO12~19 IO21~23 IO25~27
+
+
+#Except the connection between IO2 and onboard LED, other pins need to connect to external LEDs.
+
+
+
+
+
+from time import sleep
+
+
+from machine import Pin
+
+from mylib import mywifi
+
+
+
+
+ssid="ken_5G"
+password="1233211234"
+
+mywifi.connect(ssid,password)
+
+led=Pin(22,Pin.OUT)        #create LED object from pin2,Set Pin2 to output
+
+
+
+while True:
+
+
+  led.value(not led.value())
+
+
+  sleep(0.5)
+
+
+
+
+
+
+
